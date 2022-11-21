e94dde24b3f621f8b6e61abfd2cc4e24a42be557
VSAlexander <shevchenkoav.97@gmail.com>
2022-11-21 16:12:15 +0200

    only main part

--- src/index.js
+++ src/index.js
@@ -2,37 +2,59 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 
 const input = document.querySelector('#search-form');
+const {
+  elements: { searchQuery },
+} = input;
 const gallery = document.querySelector('.images-list');
+const button = document.querySelector('.load-more');
+// button.styles.display = 'none';
 
 input.addEventListener('submit', handleSubmit);
+let counter = 1;
+let previousValue = '';
 
 function handleSubmit(event) {
   event.preventDefault();
-  const {
-    elements: { searchQuery },
-  } = event.currentTarget;
-  fetchImages(searchQuery.value);
+  counter = 1;
+  clearMarkup();
+  fetchImages();
 }
 
-async function fetchImages(value) {
+async function fetchImages() {
   try {
     const response = await axios.get(`https://pixabay.com/api/`, {
       params: {
         key: '16135792-2d496ba8b681987b91053eb75',
-        q: `${value}`,
+        q: `${searchQuery.value}`,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        page: `${counter}`,
+        per_page: 40,
       },
     });
 
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    } else {
+      button.style.display = 'block';
+    }
+
     const items = response.data.hits;
+
+    if (response.data.totalHits === 0) {
+      button.style.display = 'none';
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
 
     const markup = items
       .map(
         item => `
         <li class="item">
-          <img src=${item.webformatURL} alt=${item.tags}/>
+          <img src=${item.webformatURL} alt=${item.tags} loading="lazy"/>
           <ul class="statistics-list">
             <li><p>Likes</p><span class="numbers">${item.likes}</span></li>
             <li><p>Views</p><span class="numbers">${item.views}</span></li>
@@ -44,7 +66,23 @@ async function fetchImages(value) {
       .join('');
 
     gallery.innerHTML += markup;
+    counter += 1;
+    previousValue = searchQuery.value;
   } catch (error) {
     console.log(error);
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    button.style.display = 'none';
   }
 }
+
+button.addEventListener('click', fetchImages);
+
+function clearMarkup() {
+  gallery.innerHTML = '';
+}
+
+// function handleClick() {
+//   fetchImages();
+// }
